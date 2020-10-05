a30e096a5d97433481515daede4d19ff948b3ae3
randyaajr <randyaajrsp@gmail.com>
2020-10-05 23:52:51 +0000

    Created jQuery Events/Efects. fadeTo() effect was added to the 3 cards on the index.html file

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,1 +1,29 @@
 //Here is where I'll place some jquery animate effects
+$(document).ready(function() {
+
+    $("#b-card-1").mouseenter(function() {
+        $("#card-1, #card-title-1").fadeTo(1000, 0.5);
+    });
+
+    $("#b-card-1").mouseleave(function() {
+        $("#card-1, #card-title-1").fadeTo(1000, 1)
+    });
+
+
+    $("#b-card-2").mouseenter(function() {
+        $("#card-2, #card-title-2").fadeTo(1000, 0.5);
+    });
+
+    $("#b-card-2").mouseleave(function() {
+        $("#card-2, #card-title-2").fadeTo(1000, 1)
+    });
+
+
+    $("#b-card-3").mouseenter(function() {
+        $("#card-3, #card-title-3").fadeTo(1000, 0.5);
+    });
+
+    $("#b-card-3").mouseleave(function() {
+        $("#card-3, #card-title-3").fadeTo(1000, 1)
+    });
+});
