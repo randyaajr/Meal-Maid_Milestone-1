a93bbb612f904d5749829e5580ed9fe28dc4cd2e
randyaajr <randyaajrsp@gmail.com>
2020-10-06 00:25:46 +0000

    Added method-chaining in the form of css

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -2,28 +2,28 @@
 $(document).ready(function() {
 
     $("#b-card-1").mouseenter(function() {
-        $("#card-1, #card-title-1").fadeTo(1000, 0.5);
+        $("#card-1, #card-title-1").fadeTo(1000, 0.7).css("color", "#fafafa");
     });
 
     $("#b-card-1").mouseleave(function() {
-        $("#card-1, #card-title-1").fadeTo(1000, 1)
+        $("#card-1, #card-title-1").fadeTo(1000, 1).css("color", "#3f3f3f");
     });
 
 
     $("#b-card-2").mouseenter(function() {
-        $("#card-2, #card-title-2").fadeTo(1000, 0.5);
+        $("#card-2, #card-title-2").fadeTo(1000, 0.7).css("color", "#fafafa");
     });
 
     $("#b-card-2").mouseleave(function() {
-        $("#card-2, #card-title-2").fadeTo(1000, 1)
+        $("#card-2, #card-title-2").fadeTo(1000, 1).css("color", "#3f3f3f");
     });
 
 
     $("#b-card-3").mouseenter(function() {
-        $("#card-3, #card-title-3").fadeTo(1000, 0.5);
+        $("#card-3, #card-title-3").fadeTo(1000, 0.7).css("color", "#fafafa");
     });
 
     $("#b-card-3").mouseleave(function() {
-        $("#card-3, #card-title-3").fadeTo(1000, 1)
+        $("#card-3, #card-title-3").fadeTo(1000, 1).css("color", "#3f3f3f")
     });
 });
